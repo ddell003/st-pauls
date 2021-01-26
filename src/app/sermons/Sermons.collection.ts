89498da7f478a314ef8514ed2678a35519d525dd
Parker Dell <parker.dell@maxar.com>
2021-01-26 10:21:05 -0500

    updated ideo

--- src/app/sermons/Sermons.collection.ts
+++ src/app/sermons/Sermons.collection.ts
@@ -2,8 +2,9 @@ import { Sermon } from './Sermon.model';
 
 export class SermonCollection{
     public static sermons:Sermon[] = [
-    
-        //2888839811349661
+
+        //
+        new Sermon(1715396501955868, 'I\ll Do It Tomorrow','<small>Part 4 - </small>By Rev Ryan Oswald','24 January, 2021'),
         new Sermon(502849164018845, 'I\ll Do It Tomorrow','<small>Part 3 - </small>By Rev Ryan Oswald','17 January, 2021'),
         new Sermon(2888839811349661, 'I\ll Do It Tomorrow','<small>Part 2 - </small>By Rev Ryan Oswald','10 January, 2021'),
         new Sermon(2843208045962386, 'I\ll Do It Tomorrow','<small>Part 1 - </small>By Rev Ryan Oswald','3 January, 2021'),
